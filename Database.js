7abebf73f1ce7c9642c4e65039dc688f596755fe
root <root@ip-172-31-19-223.us-west-1.compute.internal>
2019-05-03 18:50:39 +0000

    [Fix] properly end and open db connections

--- Database.js
+++ Database.js
@@ -1,14 +1,19 @@
 const mysql = require("mysql")
 class Database {
 	constructor(){
-		this.connection = mysql.createConnection({
+	}
+	connect(){
+		var config = {
 			host: "localhost",
 			user: "root",
 			database: "autoTweets"
-		});
+		};
+		this.connection = mysql.createConnection(config);
 	}
 	query(sql, args){
 		return new Promise((resolve, reject) => {
+		//need to callcreateConnection after doing end
+			this.connect();
 			this.connection.query(sql, args, (err, rows)=> {
 				if(err) return reject(err);
 				resolve(rows);
